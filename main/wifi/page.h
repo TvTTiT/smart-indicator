a026cc9ab8f7f3d98d21ddba2343a99e5431a22c
TvTTiT <82761874+TvTTiT@users.noreply.github.com>
2023-11-01 22:45:30 +0100

    modify wifi.c and create new page.c and page.h

---
+++ main/wifi/page.h
@@ -0,0 +1,7 @@
+// page.h
+#ifndef PAGE_H
+#define PAGE_H
+
+extern const char *html_welcome;
+
+#endif // PAGE_H
--- main/wifi/wifi.c
+++ main/wifi/wifi.c
@@ -1,24 +1,61 @@
 #include "wifi.h"
+#include "esp_http_server.h"
+#include "esp_log.h"
+#include "page.h"
 
 #define AP_SSID "Smart Indicator"
-#define AP_PASSWORD "12345678"
 #define AP_MAX_CONN 4
 #define AP_CHANNEL 0
-#define STA_SSID "TRY-ESP"
-#define STA_PASSWORD "12345678"
-
-static const char *html_welcome = "<html><body><h1>Welcome to Smart Indicator</h1></body></html";
 
 static esp_err_t root_handler(httpd_req_t *req) {
-    httpd_resp_set_type(req, "text/html");
-    httpd_resp_send(req, html_welcome, strlen(html_welcome));
+    httpd_resp_sendstr(req, html_welcome);
+    return ESP_OK;
+}
+
+static esp_err_t connect_handler(httpd_req_t *req) {
+    char buffer[1024];
+    int ret;
+    int received = 0;
+
+    while (1) {
+        ret = httpd_req_recv(req, buffer + received, sizeof(buffer) - received);
+        if (ret <= 0) {
+            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+                httpd_resp_send_408(req);
+            }
+            break;
+        }
+        received += ret;
+    }
+    buffer[received] = '\0';
+
+    // Parse the received data to extract SSID and password
+    char ssid[64] = "";
+    char password[64] = "";
+    if (httpd_query_key_value(buffer, "ssid", ssid, sizeof(ssid)) == ESP_OK &&
+        httpd_query_key_value(buffer, "password", password, sizeof(password)) == ESP_OK) {
+        // Print SSID and password to console
+        ESP_LOGI("WiFi", "Received SSID: %s, Password: %s", ssid, password);
+    }
+
+    // Send a "Success" response to the user
+    const char *success_response = 
+        "<html>"
+        "<body>"
+        "<h1>Success! Form Data Received</h1>"
+        "</body>"
+        "</html>";
+
+    httpd_resp_send(req, success_response, HTTPD_RESP_USE_STRLEN);
+
     return ESP_OK;
 }
 
 httpd_handle_t start_webserver(void) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    httpd_handle_t server = NULL;
+    config.max_uri_handlers = 50;
 
+    httpd_handle_t server = NULL;
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_uri_t root = {
             .uri = "/",
@@ -27,6 +64,15 @@ httpd_handle_t start_webserver(void) {
             .user_ctx = NULL,
         };
         httpd_register_uri_handler(server, &root);
+
+        httpd_uri_t connect = {
+            .uri = "/connect",
+            .method = HTTP_POST,
+            .handler = connect_handler,
+            .user_ctx = NULL,
+        };
+        httpd_register_uri_handler(server, &connect);
+
         return server;
     }
 
@@ -51,29 +97,22 @@ void wifi_initialize(void) {
 
     wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
-
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = STA_SSID,
-            .password = STA_PASSWORD,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
 
     wifi_config_t ap_config = {
         .ap = {
             .ssid = AP_SSID,
-            .password = AP_PASSWORD,
             .max_connection = AP_MAX_CONN,
             .channel = AP_CHANNEL,
             .ssid_hidden = 0,
-        },
+        }
     };
+
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
 
     ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_connect());
+
+    ESP_LOGI("WiFi", "ESP32 started in AP mode");
 
     httpd_handle_t server = start_webserver();
     if (server == NULL) {
